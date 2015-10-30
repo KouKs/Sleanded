f33215fef7ad97d7286ba82a80c223825733d17c
Pavel Koch <kouks.koch@gmail.com>
2015-10-30 18:58:44 +0100

    překopání css

--- public/js/admin.js
+++ public/js/admin.js
@@ -12,4 +12,21 @@ $(document).ready( function() {
     $("html").niceScroll({scrollspeed: 65});
     $("#port").niceScroll("#bar",{scrollspeed: 65});
     
+    /* loading with scrolling */
+    $(".area").showContent( );
+    $(this).scroll( function() {
+        $(".area").showContent( );
+    });   
 });
+
+/*
+ * Function to show area of content
+ */
+$.fn.showContent = function( ) {   
+    this.each( function( ) {
+        if( $(this).css("opacity") < 1 && $(window).scrollTop() > $(this).offset().top - 650 ) {
+            $(this).animate({opacity: 1});
+            return;
+        }
+    });
+};
